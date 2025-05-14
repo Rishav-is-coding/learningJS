86dd30ea8495a4888bcd68b209aae834f86cf770
Rishav Raj <138382043+Rishav-is-coding@users.noreply.github.com>
2025-05-14 16:35:57 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,14 @@
+"use strict";   //use all js code as newer version
+
+//data types
+//number -> 2^53
+//string
+//boolean
+//null -> standalone value
+//undefined -> abhi value assign nhi hui hai
+//symbol -> uniqueness
+
+// object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 232724 // can't be changed later 
+let accountEmail = "rishav@rishav.in"
+var accountPassword = "1234"
+accountCity = "guwahati"
+let accountState // if no value is declared then output is undifined 
+//always use let - avoid using var and no declaration
+//because of issues in block scope and functional scope
+
+console.table([accountId , accountEmail, accountPassword, accountCity, accountState])
