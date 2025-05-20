e51dca1b36a1591c7fbbfa83531e5b5754439f9d
Rishav Raj <138382043+Rishav-is-coding@users.noreply.github.com>
2025-05-20 17:25:48 +0000

    hmm

--- 04_controlFlow/two.js
+++ 04_controlFlow/two.js
@@ -62,3 +62,25 @@ console.log(total)
 */
 
 
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
